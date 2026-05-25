0b8b66b9643bdcdf3b11aaab40dbafaf225474d1
anjlikanaujiya02 <anjlikanaujiya02@gmail.com>
2026-05-25 14:59:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,49 +1,46 @@
-# VPC
+# ✅ VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "my-vpc"
   }
 }
 
-# Subnet
+# ✅ Subnet
 resource "aws_subnet" "my_subnet" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "10.0.1.0/24"
+
   tags = {
     Name = "my-subnet"
   }
 }
 
-# Internet Gateway
+# ✅ Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.my_vpc.id
 }
 
-# Route Table
+# ✅ Route Table
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.my_vpc.id
 }
 
-# Route
-resource "aws_route" "route" {
+# ✅ Route
+resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-# Associate Route Table
+# ✅ Route Table Association
 resource "aws_route_table_association" "rta" {
   subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.rt.id
 }
 
-# S3 Bucket
-resource "aws_s3_bucket" "my_bucket_1" {
-  bucket = var.bucket_name
-}
-
-# Security Group
+# ✅ Security Group
 resource "aws_security_group" "sg" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -62,7 +59,17 @@ resource "aws_security_group" "sg" {
   }
 }
 
-# EC2 Instance
+# ✅ RANDOM ID (Fix for S3 unique name)
+resource "random_id" "rand" {
+  byte_length = 4
+}
+
+# ✅ S3 BUCKET (AUTO UNIQUE ✅)
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "anjli-tf-${random_id.rand.hex}"
+}
+
+# ✅ EC2 INSTANCE (Free Tier ✅)
 resource "aws_instance" "my_ec2" {
   ami                    = var.ami
   instance_type          = var.instance_type
